9c4150863c873d122dc4d159036776f4575a9248
Gideon Dresdner <gideond@gmail.com>
2015-08-15 18:28:21 -0400

    emacs, a thing unto itself.
    put emacs config in its own repo.

--- emacs.d/config/my-javascript.el
+++
@@ -1,9 +0,0 @@
-(use-package js2-mode
-  :ensure js2-mode
-  :mode "\\.js\\'"
-  :init
-  (progn
-    (add-hook 'js-mode-hook 'js2-mode)
-    (add-hook 'js2-mode-hook 'ac-js2-mode)))
-
-(provide 'my-javascript)
